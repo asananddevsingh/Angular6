c0534dfa8f38c0ba184dd3f9a1d1c8c447f056ef
Anand Dev Singh <ananddevsingh.aiet@gmail.com>
2018-08-21 02:05:39 +0530

    Designed the recipe feature

--- food-n-flame/src/app/recipes/recipe-list/recipe-list.component.ts
+++ food-n-flame/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 
 @Component({
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {  
+export class RecipeListComponent implements OnInit {      
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
+    new Recipe('New Recipe', 'This is yummy', 'https://images.pexels.com/photos/569362/pexels-photo-569362.jpeg')
+  ];
 
   constructor() { }
 
