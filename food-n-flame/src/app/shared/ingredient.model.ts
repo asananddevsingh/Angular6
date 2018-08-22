f537f55a34c0f75ef78053f1a86a8668f0614a97
Anand Dev Singh <ananddevsingh.aiet@gmail.com>
2018-08-22 23:56:14 +0530

    Baisc design is complete
    For both recipe and shopping

---
+++ food-n-flame/src/app/shared/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient{
+    
+    // Below is the shortcut to create proerties with constructor parameters.
+    constructor(public name: string, public amount: number){
+        
+    }
+
+}
